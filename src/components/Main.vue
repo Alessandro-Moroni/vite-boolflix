98ceeed70d59078fcba374b6585dd6682afd44c6
Alessandro-Moroni <153067025+Alessandro-Moroni@users.noreply.github.com>
2024-04-03 16:04:21 +0200

    Initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<script>
+import Card from './partials/Card.vue';
+import { store } from '../data/store';
+  export default {
+    components:{
+      Card,
+    },
+    data(){
+      return{
+        store,
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
