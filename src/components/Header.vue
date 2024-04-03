7d7cae08c243a6130d222b71e301cc59621017a8
Alessandro-Moroni <alemoro98@gmail.com>
2024-04-03 16:27:48 +0200

    [first]milestone

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,12 +11,25 @@ import { store } from '../data/store';
 </script>
 
 <template>
-  <div>
+  <div class="container-fluid d-flex justify-content-between align-items-center p-3">
+    <h1>BOOLFLIX</h1>
 
+    <div class=" search-bar d-flex ">
+      <input type="text">
+      <button type="button" class="btn btn-danger mx-2">Search</button>
+    </div>
   </div>
 </template>
 
 
 <style lang="scss" scoped>
-
+.container-fluid{
+  height: 80px;
+  background-color: black;
+  position: fixed;
+  h1{
+    color: red;
+  }
+ 
+}
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -14,12 +14,31 @@ import { store } from '../data/store';
 </script>
 
 <template>
-  <div>
+  <div class="main-bc">
+    <div class="container">
+      <div class="row  row-cols-4 ">
 
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+
+      </div>
+    </div>
   </div>
 </template>
 
 
 <style lang="scss" scoped>
-
+.main-bc{
+  background-color: rgb(67, 67, 67);
+  height: 100vh;
+  padding-top: 80px;
+  .container{
+    border: 1px solid white;
+    height: 600px;
+    margin-top: 20px;
+  }
+}
 </style>
