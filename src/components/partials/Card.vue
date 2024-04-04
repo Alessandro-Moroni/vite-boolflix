1bb1c7620d75c770706548a05280db9f71effea6
Alessandro-Moroni <alemoro98@gmail.com>
2024-04-04 15:17:57 +0200

    [fix]image-not-found

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -4,12 +4,39 @@ import { store } from '../../data/store';
     props:{
       original_title: String,
       original_language: String,
-      vote_average: String,
+      vote_average: Number,
       title: String,
       poster_path: String,
     },
-   
+   data(){
+    return{
+      store,
+    }
+   },
+   methods:{
+    createStars(vote){
+      let stars = '';
+      const finalVote = vote / 2;
+      const finalStar = Math.floor(finalVote);
+      const halfStar = finalVote % 1 >= 0.5;
+      for(let i = 0; i < finalStar; i++){
+        stars += '<i class="fa-solid fa-star"></i>';
 
+      }if(halfStar){
+        stars += '<i class="fa-solid fa-star-half-stroke"></i>';
+      }
+      const emptyStar = 5;
+      for(let i = 0; i < finalStar; i++){
+
+      }if(emptyStar){
+        stars += '<i class="fa-regular fa-star"></i>';
+      }
+      
+      return stars;
+    },
+
+   },
+  
    
   }
 </script>
@@ -18,12 +45,15 @@ import { store } from '../../data/store';
   <div class="col mb-5 ">
 
     <div class="card" style="width: 18rem;">
-      <img :src="`https://image.tmdb.org/t/p/w342${poster_path} `" class="card-img-top" :alt="poster_path">
+      <img v-if="poster_path === null" src="../../assets/bandiere/img404.webp" class="card-img-top not-found" alt="">
+      <img v-else :src="`https://image.tmdb.org/t/p/w342${poster_path}`" class="card-img-top" :alt="poster_path">
       <div class="card-body">
         <h5 class="card-title">{{ original_title }}</h5>
-        <p class="card-text">{{ original_language }}</p>
+        <img class="flag" v-if="original_language === 'it'" src="../../assets/bandiere/it.png" alt="">
+        <img class="flag" v-else-if="original_language === 'en'" src="../../assets/bandiere/en.png" alt=""> 
+        <p class="card-text" v-else>{{ original_language }}</p>
         <p class="card-text">{{ title }}</p>
-        <p class="card-text">{{ vote_average }}</p>
+        <p class="card-text star" v-html="createStars(vote_average)"></p>
         
       </div>
     </div>
@@ -33,5 +63,14 @@ import { store } from '../../data/store';
 
 
 <style lang="scss" scoped>
-
+.flag{
+  width: 20%;
+}
+.star{
+  color: rgb(243, 205, 110);
+}
+.not-found{
+  height: 430px;
+  object-fit: contain;
+}
 </style>
